ba0dcc38be196b675e3a29eb95f28a4057bec0b2
sandeep199122 <sandeepkumar199122@gmail.com>
2026-09-24 16:32:18 +0530

    Update Job.js

--- models/Job.js
+++ models/Job.js
@@ -21,4 +21,4 @@ const jobSchema = new mongoose.Schema(
   { timestamps: true } // createdAt / updatedAt apne aap add ho jayega
 );
 
-module.exports = mongoose.model("Job", jobSchema);
+module.exports = mongoose.models.Job || mongoose.model("Job", jobSchema);
